2fb5856e6c6f012508fb43baa4cb1f9d7e36ddfd
MateusMenezes02 <mateusbrancomenezes@gmail.com>
2024-05-31 18:04:22 -0300

    Versão Front 3.0

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+
 import "../routes/Login.css";
 
 function Menu() {
     return(
         <div className="links">
-            <Link to="/">Home|</Link>
-            <Link to="/login">Login</Link>
+            <Link to="/login">Login|</Link> 
+            <Link to="/">Home|</Link> 
+            <Link to="/register">Register</Link>
+            
         </div>
+        
     );
 }
 export default Menu;
